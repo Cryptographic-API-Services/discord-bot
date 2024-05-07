c05f2151479095eb1b835b1ee77fd38308576c47
Mike Mulchrone <mtmulch0191@outlook.com>
2024-05-07 19:28:14 -0400

    profanity filter

--- validation/message-validation.ts
+++ validation/message-validation.ts
@@ -1,5 +1,5 @@
 import { Bot } from "https://deno.land/x/discordeno@18.0.1/bot.ts";
-import { Message, deleteMessage, sendMessage, startThreadWithoutMessage } from "https://deno.land/x/discordeno@18.0.1/mod.ts";
+import { Message, deleteMessage, sendMessage } from "https://deno.land/x/discordeno@18.0.1/mod.ts";
 
 export class MessageValidation {
     private bot: Bot;
@@ -12,16 +12,18 @@ export class MessageValidation {
 
     public validateBlackList(): boolean {
         let result = true;
-        const blacklist = ["fuck", "shit", "damn", "whore", "bastard", "nigger", "ass", "cunt", "dick", "hell"];
-        const escapedWords = blacklist.join('|');
-        const reg = new RegExp('\\b(' + escapedWords + ')\\b', 'gi');
-        if(reg.test(this.message.content.toLocaleLowerCase())) {
-            result = false;
-            deleteMessage(this.bot, this.message.channelId, this.message.id, "You used some profanity we do not allow in the channel");
-            sendMessage(this.bot, this.message.channelId, {
-                content: this.message.tag + " you have used some profanity we do not allow in the channel. As such we have deleted your message.",
-              });
+        const blacklist = ["fuck", "shit", "damn", "whore", "bastard", "nigger", "ass", "cunt", "dick"];
+        const message = this.message.content.toLowerCase();
+        for (let i = 0; i < blacklist.length; i++) {
+            if (message.includes(blacklist[i])) {
+                result = false;
+                deleteMessage(this.bot, this.message.channelId, this.message.id, "You used some profanity we do not allow in the channel");
+                sendMessage(this.bot, this.message.channelId, {
+                    content: this.message.tag + " you have used some profanity we do not allow in the channel. As such we have deleted your message.",
+                  });
+                  break;
+            }
         }
-        return result
+        return result;
     }
 }
