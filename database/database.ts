895b1becdea2b78b4fe87e1eef0685c9df603d9c
Mike Mulchrone <mtmulch0191@outlook.com>
2024-05-09 18:51:39 -0400

    increasing connection attempts

--- database/database.ts
+++ database/database.ts
@@ -1,11 +1,14 @@
 import { Pool } from "https://deno.land/x/postgres@v0.19.3/mod.ts";
 
 const pool = new Pool({
-    database: Deno.env.get("POSTGRES_DATABASE"),
-    hostname: Deno.env.get("POSTGRES_HOST"),
-    port: 5432,
-    user: Deno.env.get("POSTGRES_USER"),
-    password: Deno.env.get("POSTGRES_PASSWORD")
+  database: Deno.env.get("POSTGRES_DATABASE"),
+  hostname: Deno.env.get("POSTGRES_HOST"),
+  port: 5432,
+  user: Deno.env.get("POSTGRES_USER"),
+  password: Deno.env.get("POSTGRES_PASSWORD"),
+  connection: {
+    attempts: 10,
+  },
 }, 10);
 
 export { pool };
