e3ba45acaf19c166ea127c8df8f6ce882f983395
Mike Mulchrone <mtmulch0191@outlook.com>
2024-08-08 19:29:47 -0400

    rate limitier for the bot (#8)

--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ import {
   startBot
 } from "https://deno.land/x/discordeno@18.0.1/mod.ts";
 import MessageCreateHandler from "./handlers/message-create.ts";
+import { MessageRateLimiter } from "./handlers/message-rate-limiter.ts";
 
 const bot = createBot({
   token: Deno.env.get("DISCORD_TOKEN"),
@@ -17,9 +18,11 @@ const bot = createBot({
   },
 });
 
+const rateLimiter = new MessageRateLimiter();
+
 bot.events.messageCreate = async function (b: Bot, message: Message) {
   try {
-    const handler = new MessageCreateHandler(b, message);
+    const handler = new MessageCreateHandler(b, message, rateLimiter);
     await handler.handleMessage();
   } catch(error) {
     console.error(error);
